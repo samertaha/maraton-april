e1e57ecfc7271e50d59d829a2f4dd7f39eb40faa
Samer taha <alrazy1@gmail.com>
2024-11-16 20:56:20 +0200

    start branch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      {" "}
+      /* for styling */
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
